0044505e0d0150c01d20baecc03e17d525dc57f2
BoykoAlex <aboyko@pivotal.io>
2016-05-18 23:24:22 -0400

    Removed commented out outdated chunk of code

--- ui/app/scripts/stream/services/editor-service.js
+++ ui/app/scripts/stream/services/editor-service.js
@@ -692,40 +692,6 @@ define(function(require) {
             var type = source.attr('metadata/name');
             // NODE DROPPING TURNED OFF FOR NOW
             if (false && target instanceof joint.dia.Element && target.attr('metadata/name')) {
-                //if (type === 'tap') {
-                //    // Fill in the channel on the new node
-                //    // work out tap name, something like: tap:stream:mystream.filter (filter optional if on head of stream)
-                //    // 1. work your way back from node that was dropped on to the first node (the one with no further links)
-                //    var oncell = target;
-                //    var headerCell = oncell;
-                //    var incomingLinks = graph.getConnectedLinks(oncell, { inbound: true });
-                //    while (incomingLinks.length !== 0) {
-                //        headerCell = graph.getCell(incomingLinks[0].get('source').id);
-                //        incomingLinks = graph.getConnectedLinks(headerCell, { inbound: true });
-                //    }
-                //    var streamname = 'STREAM';
-                //    if (headerCell.attr('stream-name')) {
-                //        streamname = headerCell.attr('stream-name');
-                //    }
-                //    else {
-                //        var streamId = headerCell.attr('stream-id');
-                //        if (streamId) {
-                //            streamname = 'STREAM'+streamId;
-                //        } else {
-                //            streamname = 'STREAM';
-                //        }
-                //        headerCell.attr('stream-name',streamname);
-                //    }
-                //    var channel = 'tap:stream:'+streamname;
-                //    var label2 = 'tap:stream:\n' + streamname;
-                //    if (oncell.id !== headerCell.id) {
-                //        channel = channel + '.' + oncell.attr('.label1/text');
-                //        label2 = label2 + '.' + oncell.attr('.label1/text');
-                //    }
-                //    source.attr('.label2/text', label2);
-                //    source.attr('props/channel', channel);
-                //    relinking = true;
-                //} else {
                 if (dragDescriptor.target.selector === '.output-port') {
                     moveNodeOnNode(flo, source, target, 'right', true);
                     relinking = true;
@@ -746,7 +712,6 @@ define(function(require) {
                 //}
             } else if (target instanceof joint.dia.Link && type !== 'tap' && type !== 'destination') {
                 moveNodeOnLink(flo, source, target);
-                relinking = true;
             } else if (flo.autolink && relinking) {
                 // Search the graph for open ports - if only 1 is found, and it matches what
                 // the dropped node needs, join it up!
