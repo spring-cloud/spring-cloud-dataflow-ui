895dec29d9f08583a9b55c6403d7a1d3db8fb5c1
Gunnar Hillert <ghillert@pivotal.io>
2017-06-21 01:22:40 -0400

    Clean `ui` folder in preparation for Angular 4 rewrite

--- ui/test/protractor.conf.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
-  specs: [
-    './e2e/**/*.spec.js'
-    ],
-  baseUrl: 'http://localhost:8000',
-  onPrepare: function beforeProtractorRuns() {
-    browser.driver.manage().window().setSize(1280, 1024);
-  }
-}
--- ui/test/unit.js
+++
@@ -1,6 +0,0 @@
-define([
-    '/test/spec/jobcontrollerSpec.js',
-    '/test/spec/taskcontrollerSpec.js'
-  ], function() {
-    console.log('all tests loaded!');
-});
