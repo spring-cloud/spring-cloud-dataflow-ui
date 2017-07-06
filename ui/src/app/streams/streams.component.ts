fd85d8c8206ccbffcb5ce31e433ccc3e2844130d
Glenn Renfro <grenfro@pivotal.io>
2017-07-06 16:32:56 -0400

    Page description and title updates based on tab selection

--- ui/src/app/streams/streams.component.ts
+++ ui/src/app/streams/streams.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-streams',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StreamsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
