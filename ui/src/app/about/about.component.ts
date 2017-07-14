a0cf603b5cfc0351c0cdc23f5e8a4cdbc173b2a5
jvalkeal <janne.valkealahti@gmail.com>
2017-07-14 16:21:15 +0100

    Remove unused websocket code
    - Clean AboutComponent and remove websocket/stomp
      stuff which resulted runtime errors
    - Fixes #258

--- ui/src/app/about/about.component.ts
+++ ui/src/app/about/about.component.ts
@@ -3,7 +3,6 @@ import { AboutService } from './about.service';
 import { Subscription } from 'rxjs/Subscription';
 import { BusyModule, BusyDirective } from 'angular2-busy';
 import { ToastyService, ToastyConfig, ToastOptions, ToastData } from 'ng2-toasty';
-import { StompService } from 'ng2-stomp-service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
@@ -14,53 +13,13 @@ export class AboutComponent implements OnInit {
 
   dataflowVersionInfo;
   busy: Subscription;
-  websocketData;
 
   private subscription: any;
 
   constructor(private aboutService: AboutService, private toastyService: ToastyService,
-              stomp: StompService, private router: Router) {
+              private router: Router) {
 
-    stomp.configure({
-      host: '/websocket',
-      debug: true,
-      queue: { 'init': false }
-    });
-
-    // start connection
-    stomp.startConnect().then(() => {
-      stomp.done('init');
-      console.log('connected');
-
-      // subscribe
-      // this.subscription = stomp.subscribe('/topic/price.stock.*', this.response);
-
-      // this.subscription = stomp.subscribe('/topic/updates', this.response);
-
-      // send data
-      // stomp.send('destionation',{"data":"data"});
-
-      // unsubscribe
-      // this.subscription.unsubscribe();
-
-      // disconnect
-      // stomp.disconnect().then(() => {
-      // console.log( 'Connection closed' )
-      // })
-    });
   }
-
-  stopWebsockets() {
-    console.log('Unsubscribing...');
-    this.subscription.unsubscribe('/topic/updates');
-  }
-
-  public response = (data) => {
-    console.log(data);
-    this.websocketData = data.message;
-    this.toastyService.warning(`Time ${data.message} loaded.`);
-  }
-
   ngOnInit() {
     console.log('init');
     this.getVersionInfo();
