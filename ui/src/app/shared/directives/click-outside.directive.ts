e45cd3e3f38d2664eb21983f8f3fdc075573228f
Gunnar Hillert <ghillert@pivotal.io>
2017-07-25 08:07:51 -1000

    gh-279 Fix click-outside-directive

--- ui/src/app/shared/directives/click-outside.directive.ts
+++ ui/src/app/shared/directives/click-outside.directive.ts
@@ -1,25 +1,33 @@
 import { Directive, ElementRef, Output, EventEmitter, HostListener } from '@angular/core';
 
+/**
+ * This directive is a helper for any situation where you need
+ * to close pop-overs etc. by means of clicking outside of the
+ * element.
+ *
+ * @author Gunnar Hillert
+ */
 @Directive({
     selector: '[appClickOutside]'
 })
 export class ClickOutsideDirective {
 
     @Output()
-    public clickOutside = new EventEmitter<MouseEvent>();
+    public appClickOutside = new EventEmitter<MouseEvent>();
 
     constructor(private _elementRef: ElementRef) {
     }
 
     @HostListener('document:click', ['$event', '$event.target'])
     public onClick(event: MouseEvent, targetElement: HTMLElement): void {
+        
         if (!targetElement) {
             return;
         }
 
         const clickedInside = this._elementRef.nativeElement.contains(targetElement);
         if (!clickedInside) {
-            this.clickOutside.emit(event);
+            this.appClickOutside.emit(event);
         }
     }
 }
