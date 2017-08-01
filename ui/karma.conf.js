825d206091d207a6a2e3f360f676ca6e3e950d40
Gunnar Hillert <ghillert@pivotal.io>
2017-08-01 13:59:45 -1000

    gh-285 Fix Production bundle error
    - Temporarily upgrade to `tixif-ngx-busy`
    - Upgrade to Angular Cli `1.3.0 RC3`

--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
@@ -8,10 +8,11 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+
+      require('karma-phantomjs-launcher')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
