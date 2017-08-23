8351eefc594e7c51f3af5e9eb39e5753f77b21db
BoykoAlex <aboyko@pivotal.io>
2017-08-23 19:14:15 -0400

    Rough-in for Streams editor Flo services.

--- ui/src/app/streams/stream-create/stream-create.component.ts
+++ ui/src/app/streams/stream-create/stream-create.component.ts
@@ -1,35 +1,11 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Flo } from 'spring-flo';
-
-
-export class Metamodel implements Flo.Metamodel {
-
-  constructor() {
-  }
-
-  textToGraph(flo: Flo.EditorContext, dsl: string) {
-  }
-
-  graphToText(flo: Flo.EditorContext) {
-    console.log('Graph -> Text');
-    return Promise.resolve('');
-  }
-
-  load(): Promise < Map < string, Map < string, Flo.ElementMetadata >>> {
-    const data = new Map();
-    return Promise.resolve(data);
-  }
-
-  groups(): Array < string > {
-    return [];
-  }
-
-}
+import { MetamodelService } from '../flo/metamodel.service';
 
 @Component({
   selector: 'app-stream-create',
   templateUrl: './stream-create.component.html',
-  styleUrls: [ './flo.css' ],
+  styleUrls: [ '../flo/flo.css' ],
   encapsulation: ViewEncapsulation.None
 
 })
@@ -39,11 +15,11 @@ export class StreamCreateComponent implements OnInit {
 
   editorContext: Flo.EditorContext;
 
-  metamodel = new Metamodel();
-
   paletteSize = 170;
 
-  constructor() { }
+  constructor(public metamodel : MetamodelService) {
+    console.log('Building');
+  }
 
   ngOnInit() {
   }
