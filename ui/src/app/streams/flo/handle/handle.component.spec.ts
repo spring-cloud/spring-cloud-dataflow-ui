ad3c7c96c2abe60a68cfaf5c5674ddf3de317aa8
Janne Valkealahti <janne.valkealahti@gmail.com>
2017-09-12 18:39:17 +0100

    Angular boostrap tooltips for JointJS shapes
    - Tooltips for nodes
    - Tooltips for JointJS shapes
    - Tooltips for JointJS shapes
    - Fixes #438

---
+++ ui/src/app/streams/flo/handle/handle.component.spec.ts
@@ -0,0 +1,77 @@
+import { Shapes } from 'spring-flo';
+import { dia } from 'jointjs';
+import { RenderService } from '../render.service';
+import { MockMetamodelService } from '../mocks/mock-metamodel.service';
+import { HandleComponent } from './handle.component';
+import { Constants } from 'spring-flo';
+
+import * as _joint from 'jointjs';
+const joint: any = _joint;
+
+function createMockView(cell: dia.Element): any {
+  return {
+    model: cell,
+  };
+}
+
+describe('HandleComponent Tests.', () => {
+
+  const MOCK_METAMODEL_SERVICE = new MockMetamodelService();
+  const RENDER_SERVICE = new RenderService(MOCK_METAMODEL_SERVICE);
+
+  let graph: dia.Graph;
+  let component: HandleComponent;
+  let parentNode: dia.Element;
+
+  beforeEach(() => {
+    graph = new joint.dia.Graph();
+    component = new HandleComponent();
+    parentNode = Shapes.Factory.createNode({
+      metadata: MOCK_METAMODEL_SERVICE.data.get('source').get('http'),
+      renderer: RENDER_SERVICE,
+      graph: graph
+    });
+  });
+
+  it('No handle view', () => {
+    expect(component.placement).toEqual('top');
+    expect(component.tooltipText).toBeUndefined();
+  });
+
+  it('Remove Handle', () => {
+    const removeHandle = Shapes.Factory.createHandle({
+      kind: Constants.REMOVE_HANDLE_TYPE,
+      parent: parentNode,
+      renderer: RENDER_SERVICE,
+      graph: graph
+    });
+    component.view = createMockView(removeHandle);
+    expect(component.placement).toEqual('bottom');
+    expect(component.tooltipText).toEqual('Remove Element');
+  });
+
+  it('Edit Properties Handle', () => {
+    const propertiesHandle = Shapes.Factory.createHandle({
+      kind: Constants.PROPERTIES_HANDLE_TYPE,
+      parent: parentNode,
+      renderer: RENDER_SERVICE,
+      graph: graph
+    });
+    component.view = createMockView(propertiesHandle);
+    expect(component.placement).toEqual('bottom');
+    expect(component.tooltipText).toEqual('Edit Properties');
+  });
+
+  it('Random Handle', () => {
+    const randomHandle = Shapes.Factory.createHandle({
+      kind: 'random',
+      parent: parentNode,
+      renderer: RENDER_SERVICE,
+      graph: graph
+    });
+    component.view = createMockView(randomHandle);
+    expect(component.placement).toEqual('top');
+    expect(component.tooltipText).toBeUndefined();
+  });
+
+});
