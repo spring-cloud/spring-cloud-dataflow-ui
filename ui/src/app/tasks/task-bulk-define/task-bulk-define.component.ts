8ee24635a0f296959f60eaa01a126e48cb3bcef5
Gunnar Hillert <ghillert@pivotal.io>
2017-10-02 22:50:31 -1000

    gh-325 Fix: Validation not working for bulk task definitions

--- ui/src/app/tasks/task-bulk-define/task-bulk-define.component.ts
+++ ui/src/app/tasks/task-bulk-define/task-bulk-define.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+
+import { Observable } from 'rxjs/Observable';
+
 import { TasksService } from '../tasks.service';
+import { ToastyService } from 'ng2-toasty';
+
 import { validateBulkTaskDefinitions } from './task-bulk-define-validators';
 
 @Component({
@@ -15,6 +20,7 @@ export class TaskBulkDefineComponent implements OnInit {
 
   constructor(
     private tasksService: TasksService,
+    private toastyService: ToastyService,
     private router: Router,
     private fb: FormBuilder
   ) {
@@ -31,18 +37,50 @@ export class TaskBulkDefineComponent implements OnInit {
   }
 
   bulkDefineTasks() {
-    console.log('bulkDefineTasks');
+    const definitions = this.definitions.value.split('\n');
+
+    const observables: Observable<Response>[] = [];
+
+    console.log(`Bulk-Define ${definitions.length} Task(s)`);
     if (this.definitions.value) {
-      for (const def of this.definitions.value.split('\n')) {
+      for (const def of definitions) {
         if (def.length > 2) {
           const keyValue = def.split('=');
           if (keyValue.length === 2) {
-            this.tasksService.createDefinition(keyValue[1], keyValue[0]).subscribe();
+            const observable = this.tasksService.createDefinition(keyValue[1], keyValue[0])
+            .catch(error => {
+              return Observable.of({
+                isError: true,
+                errorMessage: error
+              });
+            });
+            observables.push(observable);
           }
         }
       }
     }
-    this.router.navigate(['tasks/definitions']);
+    Observable.forkJoin(observables).subscribe(
+      data => {
+        let completed = 0;
+        let failed = 0;
+        for (const dataItem of data as any) {
+          if (dataItem.isError) {
+            this.toastyService.error(dataItem.errorMessage);
+            failed++;
+          } else {
+            completed++;
+          }
+        }
+        if (failed === 0) {
+          this.toastyService.success(`${data.length} task(s) defined.`);
+          this.router.navigate(['tasks/definitions']);
+        } else if (failed > 0 && completed > 0) {
+          this.toastyService.error(`${failed} of ${data.length} task(s) could not be defined.`);
+        } else {
+          this.toastyService.error(`All ${data.length} task(s) could not be defined.`);
+        }
+      }
+    );
   }
 
   /**
