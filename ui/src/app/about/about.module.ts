9837edfdcbd0af94bf51401aff9a8eed9e513df8
Damien Vitrac <dvitrac@pivotal.io>
2018-06-04 17:33:29 +0200

    Refactoring About Module

--- ui/src/app/about/about.module.ts
+++ ui/src/app/about/about.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
-import { AboutComponent } from './about.component';
-import { AboutDetailsComponent } from './about-details.component';
+import { AboutComponent } from './about/about.component';
+import { AboutDetailsComponent } from './components/about-more/about-details.component';
 import { AboutService } from './about.service';
 import { AboutRoutingModule } from './about-routing.module';
 
 import { ClipboardModule } from 'ngx-clipboard';
 
 @NgModule({
-  imports:      [ AboutRoutingModule, SharedModule, ClipboardModule ],
-  declarations: [ AboutComponent, AboutDetailsComponent ],
-  providers:    [ AboutService ]
+  imports: [
+    AboutRoutingModule,
+    SharedModule,
+    ClipboardModule
+  ],
+  declarations: [
+    AboutComponent,
+    AboutDetailsComponent
+  ],
+  providers: [
+    AboutService
+  ]
 })
-export class AboutModule { }
+export class AboutModule {
+}
--- ui/src/app/about/about.service.ts
+++ ui/src/app/about/about.service.ts
@@ -5,7 +5,7 @@ import { Subject } from 'rxjs/Subject';
 
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
-import {ErrorHandler} from '../shared/model/error-handler';
+import { ErrorHandler } from '../shared/model/error-handler';
 
 import { SharedAboutService } from '../shared/services/shared-about.service';
 import { FeatureInfo } from '../shared/model/about/feature-info.model';
@@ -14,26 +14,59 @@ import { AboutInfo } from '../shared/model/about/about-info.model';
 @Injectable()
 export class AboutService {
 
-  constructor(
-    private sharedAboutService: SharedAboutService,
-    private http: Http, private errorHandler: ErrorHandler) {}
+  /**
+   * Constructor
+   *
+   * @param {SharedAboutService} sharedAboutService
+   * @param {Http} http
+   * @param {ErrorHandler} errorHandler
+   */
+  constructor(private sharedAboutService: SharedAboutService,
+              private http: Http,
+              private errorHandler: ErrorHandler) {
+  }
 
+  /**
+   * Get About info
+   *
+   * @returns {AboutInfo}
+   */
   public get aboutInfo(): AboutInfo {
     return this.sharedAboutService.aboutInfo;
   }
 
+  /**
+   * Get Feature Info
+   *
+   * @returns {FeatureInfo}
+   */
   public get featureInfo(): FeatureInfo {
     return this.sharedAboutService.featureInfo;
   }
 
+  /**
+   * Set Feature info
+   *
+   * @param {FeatureInfo} featureInfo
+   */
   public set featureInfo(featureInfo: FeatureInfo) {
     this.sharedAboutService.featureInfo = featureInfo;
   }
 
+  /**
+   * Get Feature Info Subject
+   * @returns {Subject<FeatureInfo>}
+   */
   public get featureInfoSubject(): Subject<FeatureInfo> {
     return this.sharedAboutService.featureInfoSubject;
   }
 
+  /**
+   * Get Info
+   *
+   * @param {boolean} reload
+   * @returns {Observable<AboutInfo>}
+   */
   getAboutInfo(reload?: boolean): Observable<AboutInfo> {
     if (reload) {
       this.sharedAboutService.loadAboutInfo(reload);
@@ -41,7 +74,12 @@ export class AboutService {
     return this.sharedAboutService.getAboutInfo();
   }
 
+  /**
+   * Get details
+   * @returns {Observable<AboutInfo>}
+   */
   getDetails(): Observable<AboutInfo> {
     return this.sharedAboutService.getAboutInfo();
   }
+
 }
