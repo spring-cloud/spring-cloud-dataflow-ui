0f438af8bc81c82243c9d216ed64d63d57fb4c11
Damien Vitrac <dvitrac@pivotal.io>
2019-03-25 18:53:19 +0100

    Stream creation: add custom URL params encoder
    Resolve #1152 #1074

---
+++ ui/src/app/shared/support/encoder.utils.ts
@@ -0,0 +1,24 @@
+import { HttpParameterCodec } from '@angular/common/http';
+
+/**
+ * Custom HTTP Parameter Encoder
+ */
+export class DataflowEncoder implements HttpParameterCodec {
+
+  encodeKey(key: string): string {
+    return encodeURIComponent(key);
+  }
+
+  encodeValue(value: string): string {
+    return encodeURIComponent(value);
+  }
+
+  decodeKey(key: string): string {
+    return decodeURIComponent(key);
+  }
+
+  decodeValue(value: string): string {
+    return decodeURIComponent(value);
+  }
+
+}
