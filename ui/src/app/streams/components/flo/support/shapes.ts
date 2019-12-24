a6b6cbd35f5a79d60e0809f43f6ea5c9b97cace0
BoykoAlex <aboyko@pivotal.io>
2019-12-24 15:46:24 -0500

    Remove link properties action from link

--- ui/src/app/streams/components/flo/support/shapes.ts
+++ ui/src/app/streams/components/flo/support/shapes.ts
@@ -124,55 +124,6 @@ joint.shapes.flo.DataFlowApp = joint.shapes.basic.Generic.extend({
         rx: NODE_ROUNDED_CORNER,
         ry: NODE_ROUNDED_CORNER,
       },
-      '.select-1': {
-        width: SELECT_SQUARE_SIZE,
-        height: SELECT_SQUARE_SIZE,
-        ref: '.box',
-        refX: SELECT_SQUARE_SIZE,
-        refY: -SELECT_SQUARE_SIZE / 2,
-      },
-      '.select-2': {
-        width: SELECT_SQUARE_SIZE,
-        height: SELECT_SQUARE_SIZE,
-        ref: '.box',
-        refX: '50%',
-        refX2: -SELECT_SQUARE_SIZE / 2,
-        refY: -SELECT_SQUARE_SIZE / 2
-      },
-      '.select-3': {
-        width: SELECT_SQUARE_SIZE,
-        height: SELECT_SQUARE_SIZE,
-        ref: '.box',
-        refX: '100%',
-        refX2: -SELECT_SQUARE_SIZE - 8,
-        refY: -SELECT_SQUARE_SIZE / 2
-      },
-      '.select-4': {
-        width: SELECT_SQUARE_SIZE,
-        height: SELECT_SQUARE_SIZE,
-        ref: '.box',
-        refX: SELECT_SQUARE_SIZE,
-        refY: '100%',
-        refY2: -SELECT_SQUARE_SIZE / 2,
-      },
-      '.select-5': {
-        width: SELECT_SQUARE_SIZE,
-        height: SELECT_SQUARE_SIZE,
-        ref: '.box',
-        refX: '50%',
-        refX2: -SELECT_SQUARE_SIZE / 2,
-        refY: '100%',
-        refY2: -SELECT_SQUARE_SIZE / 2,
-      },
-      '.select-6': {
-        width: SELECT_SQUARE_SIZE,
-        height: SELECT_SQUARE_SIZE,
-        ref: '.box',
-        refX: '100%',
-        refX2: -SELECT_SQUARE_SIZE - 8,
-        refY: '100%',
-        refY2: -SELECT_SQUARE_SIZE / 2,
-      },
       '.shape': {}
     }
   }, joint.shapes.basic.Generic.prototype.defaults)
@@ -209,16 +160,6 @@ joint.shapes.flo.Destination = joint.shapes.basic.Generic.extend({
         fill: 'black',
         'font-size': 14
       },
-      // '.name-label': {
-      //   'refX': 0.5, // jointjs specific: relative position to ref'd element
-      //   'refY': 0.5,
-      //   'refY2': -2,
-      //   'y-alignment': 'bottom',
-      //   'x-alignment': 'middle',
-      //   ref: '.box', // jointjs specific: element for ref-x, ref-y
-      //   fill: 'black',
-      //   'font-size': 14
-      // },
       '.name-label': {
         'refX': 10 + TYPE_ICON_SIZE.width + 10, // jointjs specific: relative position to ref'd element
         'refY': 0.5,
@@ -280,8 +221,10 @@ joint.shapes.flo.LinkDataflow = joint.shapes.flo.Link.extend({
 
   toolMarkup: [
     '<g class="link-tool create-stream">',
-    '<rect class="link-tools-container" width="28" height="28"/>',
-    '<g class="tool-remove" event="remove" transform="translate(8, 4)">',
+    // '<rect class="link-tools-container" width="28" height="28"/>',
+    // '<g class="tool-remove" event="remove" transform="translate(8, 4)">',
+    '<rect class="link-tools-container" width="28" height="28" transform="translate(-20 -20)"/>',
+    '<g class="tool-remove" event="remove">',
     '<circle r="11" transform="scale(.7)"/>',
     '<path transform="scale(.6) translate(-16, -16)" d="M24.778,21.419 19.276,15.917 24.777,10.415 21.949,7.585' +
     ' 16.447,13.087 10.945,7.585 8.117,10.415 13.618,15.917 8.116,21.419 10.946,24.248 16.447,18.746 21.948,24.248z"/>',
@@ -302,21 +245,20 @@ joint.shapes.flo.LinkDataflow = joint.shapes.flo.Link.extend({
     ' style="stroke:rgb(0,0,0);stroke-width:2"/>',
     '<title>Switch to/from tap</title>',
     '</g>',
-    '<g class="tool-options" event="link:options" transform="translate(-17, -4)">',
-    // '<circle r="11" transform="scale(.7)"/>',
-    '<path fill="white" transform="scale(.50)" d="M31.229,17.736c0.064-0.571,0.104-1.148,0.104-' +
-    '1.736s-0.04-1.166-0.104-1.737l-4.377-1.557c-0.218-0.716-0.504-1.401-0.851-2.05l1.993-4.192c-0.725-0.91-' +
-    '1.549-1.734-2.458-2.459l-4.193,1.994c-0.647-0.347-1.334-0.632-2.049-0.849l-1.558-4.378C17.165,0.708,16.588,' +
-    '0.667,16,0.667s-1.166,0.041-1.737,0.105L12.707,5.15c-0.716,0.217-1.401,0.502-2.05,0.849L6.464,4.005C5.554,' +
-    '4.73,4.73,5.554,4.005,6.464l1.994,4.192c-0.347,0.648-0.632,1.334-0.849,2.05l-4.378,1.557C0.708,14.834,0.667,' +
-    '15.412,0.667,16s0.041,1.165,0.105,1.736l4.378,1.558c0.217,0.715,0.502,1.401,0.849,2.049l-1.994,4.193c0.725,' +
-    '0.909,1.549,1.733,2.459,2.458l4.192-1.993c0.648,0.347,1.334,0.633,2.05,0.851l1.557,4.377c0.571,0.064,1.148,' +
-    '0.104,1.737,0.104c0.588,0,1.165-0.04,1.736-0.104l1.558-4.377c0.715-0.218,1.399-0.504,2.049-0.851l4.193,' +
-    '1.993c0.909-0.725,1.733-1.549,2.458-2.458l-1.993-4.193c0.347-0.647,0.633-1.334,0.851-2.049L31.229,17.736zM16,' +
-    '20.871c-2.69,0-4.872-2.182-4.872-4.871c0-2.69,2.182-4.872,4.872-4.872c2.689,0,4.871,2.182,4.871,4.872C20.871,' +
-    '18.689,18.689,20.871,16,20.871z"/>',
-    '<title>Properties</title>',
-    '</g>',
+    // '<g class="tool-options" event="link:options" transform="translate(-17, -4)">',
+    // '<path fill="white" transform="scale(.50)" d="M31.229,17.736c0.064-0.571,0.104-1.148,0.104-' +
+    // '1.736s-0.04-1.166-0.104-1.737l-4.377-1.557c-0.218-0.716-0.504-1.401-0.851-2.05l1.993-4.192c-0.725-0.91-' +
+    // '1.549-1.734-2.458-2.459l-4.193,1.994c-0.647-0.347-1.334-0.632-2.049-0.849l-1.558-4.378C17.165,0.708,16.588,' +
+    // '0.667,16,0.667s-1.166,0.041-1.737,0.105L12.707,5.15c-0.716,0.217-1.401,0.502-2.05,0.849L6.464,4.005C5.554,' +
+    // '4.73,4.73,5.554,4.005,6.464l1.994,4.192c-0.347,0.648-0.632,1.334-0.849,2.05l-4.378,1.557C0.708,14.834,0.667,' +
+    // '15.412,0.667,16s0.041,1.165,0.105,1.736l4.378,1.558c0.217,0.715,0.502,1.401,0.849,2.049l-1.994,4.193c0.725,' +
+    // '0.909,1.549,1.733,2.459,2.458l4.192-1.993c0.648,0.347,1.334,0.633,2.05,0.851l1.557,4.377c0.571,0.064,1.148,' +
+    // '0.104,1.737,0.104c0.588,0,1.165-0.04,1.736-0.104l1.558-4.377c0.715-0.218,1.399-0.504,2.049-0.851l4.193,' +
+    // '1.993c0.909-0.725,1.733-1.549,2.458-2.458l-1.993-4.193c0.347-0.647,0.633-1.334,0.851-2.049L31.229,17.736zM16,' +
+    // '20.871c-2.69,0-4.872-2.182-4.872-4.871c0-2.69,2.182-4.872,4.872-4.872c2.689,0,4.871,2.182,4.871,4.872C20.871,' +
+    // '18.689,18.689,20.871,16,20.871z"/>',
+    // '<title>Properties</title>',
+    // '</g>',
     '</g>'
   ].join(''),
 
