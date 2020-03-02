dd463b008f8cb4be570af71990e5c6edacd99dda
Damien Vitrac <dvitrac@pivotal.io>
2020-03-02 17:39:22 +0100

    Update URL for stream runtime metrics

--- ui/src/app/streams/streams.service.spec.ts
+++ ui/src/app/streams/streams.service.spec.ts
@@ -17,7 +17,7 @@ describe('StreamsService', () => {
   let mockHttp;
   let jsonData;
   let streamsService;
-  
+
   beforeEach(() => {
     mockHttp = {
       delete: jasmine.createSpy('delete'),
@@ -307,7 +307,7 @@ describe('StreamsService', () => {
       streamsService.getRuntimeStreamStatuses();
       const httpUri = mockHttp.get.calls.mostRecent().args[0];
       const headerArgs = mockHttp.get.calls.mostRecent().args[1].headers;
-      expect(httpUri).toEqual('/runtime/streams');
+      expect(httpUri).toEqual('/runtime/streams/status');
       expect(headerArgs.get('Content-Type')).toEqual('application/json');
       expect(headerArgs.get('Accept')).toEqual('application/json');
     });
@@ -322,7 +322,7 @@ describe('StreamsService', () => {
       const httpUri = mockHttp.get.calls.mostRecent().args[0];
       const headerArgs = mockHttp.get.calls.mostRecent().args[1].headers;
       const httpParams = mockHttp.get.calls.mostRecent().args[1].params;
-      expect(httpUri).toEqual('/runtime/streams');
+      expect(httpUri).toEqual('/runtime/streams/status');
       expect(headerArgs.get('Content-Type')).toEqual('application/json');
       expect(headerArgs.get('Accept')).toEqual('application/json');
       expect(httpParams.get('names')).toEqual('test1,test2');
