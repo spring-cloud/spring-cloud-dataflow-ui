49b792d1ad98a51d5930df48cda8e5734c5781ec
Damien Vitrac <dvitrac@pivotal.io>
2020-03-02 19:18:53 +0100

    Update runtime app model parsing

--- ui/src/app/runtime/model/runtime-app.ts
+++ ui/src/app/runtime/model/runtime-app.ts
@@ -25,7 +25,7 @@ export class RuntimeApp {
 
   public static fromJSON(input): RuntimeApp {
     let instances = [];
-    if (input.instances._embedded.appInstanceStatusResourceList) {
+    if (!!input.instances && !!input.instances._embedded && !!input.instances._embedded.appInstanceStatusResourceList) {
       instances = input.instances._embedded.appInstanceStatusResourceList;
     }
     return new RuntimeApp(input.deploymentId, input.state, input.instances, instances);
@@ -35,7 +35,7 @@ export class RuntimeApp {
     const page = Page.fromJSON<RuntimeApp>(input);
     if (input && input._embedded && input._embedded.appStatusResourceList) {
       page.items = input._embedded.appStatusResourceList.map((item) => {
-        if (!!item.instances._embedded && !!item.instances._embedded.appInstanceStatusResourceList) {
+        if (!!item.instances && !!item.instances._embedded && !!item.instances._embedded.appInstanceStatusResourceList) {
           item.appInstances = item.instances._embedded.appInstanceStatusResourceList;
         } else {
           item.appInstances = [];
