da135bacb070466ba425e839e77876fa5cd9ad5e
Damien Vitrac <dvitrac@pivotal.io>
2020-06-08 15:09:23 +0200

    Update interceptor security

--- ui/src/app/security/support/token.interceptor.ts
+++ ui/src/app/security/support/token.interceptor.ts
@@ -2,7 +2,6 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Injectable, Injector } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SecurityService } from '../service/security.service';
-import { delay } from 'rxjs/operators';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
@@ -19,9 +18,6 @@ export class TokenInterceptor implements HttpInterceptor {
         }
       });
     }
-    if (['/security/info', '/about'].indexOf(request.url) > -1) {
-      return next.handle(request);
-    }
-    return next.handle(request).pipe(delay(2000));
+    return next.handle(request);
   }
 }
