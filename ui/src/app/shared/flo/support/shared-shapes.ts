30d9158f928f9b9466268989a48d77d4195bfdb9
BoykoAlex <aboyko@pivotal.io>
2020-06-16 11:20:13 -0400

    Load shapes function

--- ui/src/app/shared/flo/support/shared-shapes.ts
+++ ui/src/app/shared/flo/support/shared-shapes.ts
@@ -1,126 +1,134 @@
 import { dia } from 'jointjs';
 import * as _joint from 'jointjs';
 
+import { loadShapes } from 'spring-flo';
+
 const joint: any = _joint;
 const g = joint.g;
 const V = joint.V;
 
-joint.connectors.smoothHorizontal = function (sourcePoint, targetPoint, route, opt) {
+export function loadSharedShapes() {
 
-  const raw = opt && opt.raw;
-  let path;
+  // Load shapes from JointJS and Flo
+  loadShapes();
 
-  if (route && route.length !== 0) {
+  joint.connectors.smoothHorizontal = function (sourcePoint, targetPoint, route, opt) {
 
-    const points = [sourcePoint].concat(route).concat([targetPoint]);
-    const curves = g.Curve.throughPoints(points);
+    const raw = opt && opt.raw;
+    let path;
 
-    path = new g.Path(curves);
+    if (route && route.length !== 0) {
 
-  } else {
-    // if we have no route, use a default cubic bezier curve
-    // cubic bezier requires two control points
-    // the control points have `x` midway between source and target
-    // this produces an S-like curve
+      const points = [sourcePoint].concat(route).concat([targetPoint]);
+      const curves = g.Curve.throughPoints(points);
 
-    path = new g.Path();
+      path = new g.Path(curves);
 
-    let segment;
+    } else {
+      // if we have no route, use a default cubic bezier curve
+      // cubic bezier requires two control points
+      // the control points have `x` midway between source and target
+      // this produces an S-like curve
 
-    segment = g.Path.createSegment('M', sourcePoint);
-    path.appendSegment(segment);
+      path = new g.Path();
 
-    const controlPointX = (sourcePoint.x + targetPoint.x) / 2;
+      let segment;
 
-    segment = g.Path.createSegment('C', controlPointX, sourcePoint.y, controlPointX, targetPoint.y, targetPoint.x, targetPoint.y);
-    path.appendSegment(segment);
-  }
+      segment = g.Path.createSegment('M', sourcePoint);
+      path.appendSegment(segment);
 
-  return (raw) ? path : path.serialize();
-};
+      const controlPointX = (sourcePoint.x + targetPoint.x) / 2;
 
-joint.connectors.smoothVertical = function (sourcePoint, targetPoint, route, opt) {
+      segment = g.Path.createSegment('C', controlPointX, sourcePoint.y, controlPointX, targetPoint.y, targetPoint.x, targetPoint.y);
+      path.appendSegment(segment);
+    }
 
-  const raw = opt && opt.raw;
-  let path;
+    return (raw) ? path : path.serialize();
+  };
 
-  if (route && route.length !== 0) {
+  joint.connectors.smoothVertical = function (sourcePoint, targetPoint, route, opt) {
 
-    const points = [sourcePoint].concat(route).concat([targetPoint]);
-    const curves = g.Curve.throughPoints(points);
+    const raw = opt && opt.raw;
+    let path;
 
-    path = new g.Path(curves);
+    if (route && route.length !== 0) {
 
-  } else {
-    // if we have no route, use a default cubic bezier curve
-    // cubic bezier requires two control points
-    // the control points have `x` midway between source and target
-    // this produces an S-like curve
+      const points = [sourcePoint].concat(route).concat([targetPoint]);
+      const curves = g.Curve.throughPoints(points);
 
-    path = new g.Path();
+      path = new g.Path(curves);
 
-    let segment;
+    } else {
+      // if we have no route, use a default cubic bezier curve
+      // cubic bezier requires two control points
+      // the control points have `x` midway between source and target
+      // this produces an S-like curve
 
-    segment = g.Path.createSegment('M', sourcePoint);
-    path.appendSegment(segment);
+      path = new g.Path();
 
-    const controlPointY = (sourcePoint.y + targetPoint.y) / 2;
+      let segment;
 
-    segment = g.Path.createSegment('C', sourcePoint.x, controlPointY, targetPoint.x, controlPointY, targetPoint.x, targetPoint.y);
-    path.appendSegment(segment);
-  }
+      segment = g.Path.createSegment('M', sourcePoint);
+      path.appendSegment(segment);
 
-  return (raw) ? path : path.serialize();
-};
+      const controlPointY = (sourcePoint.y + targetPoint.y) / 2;
 
-joint.shapes.flo.DataflowPaletteGroupHeader = joint.shapes.basic.Generic.extend({
-  // The path is the open/close arrow, defaults to vertical (open)
-  markup: '<g><rect class="outer"/><rect class="group-label-bg"/><text class="group-label"/><image class="collapse-handle"/><polyline class="group-line"/></g>',
-  defaults: joint.util.deepSupplement({
-    type: 'palette.groupheader',
-    size: {width: 170, height: 40},
-    position: {x: 0, y: 0},
-    attrs: {
-      '.outer': {
-        refWidth: 1,
-        refHeight: 8,
-        refX: 0,
-        refY: 0,
+      segment = g.Path.createSegment('C', sourcePoint.x, controlPointY, targetPoint.x, controlPointY, targetPoint.x, targetPoint.y);
+      path.appendSegment(segment);
+    }
+
+    return (raw) ? path : path.serialize();
+  };
+
+  joint.shapes.flo.DataflowPaletteGroupHeader = joint.shapes.basic.Generic.extend({
+    // The path is the open/close arrow, defaults to vertical (open)
+    markup: '<g><rect class="outer"/><rect class="group-label-bg"/><text class="group-label"/><image class="collapse-handle"/><polyline class="group-line"/></g>',
+    defaults: joint.util.deepSupplement({
+      type: 'palette.groupheader',
+      size: {width: 170, height: 40},
+      position: {x: 0, y: 0},
+      attrs: {
+        '.outer': {
+          refWidth: 1,
+          refHeight: 8,
+          refX: 0,
+          refY: 0,
+        },
+        '.group-label-bg': {
+          ref: '.group-label',
+          refWidth: 20,
+          refHeight: 6,
+          refX: -10,
+          refY: -3,
+          rx: 10,
+          ry: 10,
+          'follow-scale': true,
+        },
+        '.group-label': {
+          ref: '.outer',
+          refX: 10,
+          refY: 0.5,
+          'y-alignment': 'middle'
+        },
+        '.collapse-handle': {
+          width: 30,
+          height: 14,
+          ref: '.outer',
+          refX: '100%',
+          refX2: '-30 - 20',
+          refY: '50%',
+          refY2: -5
+        },
+        '.group-line': {
+          ref: '.outer',
+          refPointsKeepOffset: '1,42 169,42'
+        }
       },
-      '.group-label-bg': {
-        ref: '.group-label',
-        refWidth: 20,
-        refHeight: 6,
-        refX: -10,
-        refY: -3,
-        rx: 10,
-        ry: 10,
-        'follow-scale': true,
-      },
-      '.group-label': {
-        ref: '.outer',
-        refX: 10,
-        refY: 0.5,
-        'y-alignment': 'middle'
-      },
-      '.collapse-handle': {
-        width: 30,
-        height: 14,
-        ref: '.outer',
-        refX: '100%',
-        refX2: '-30 - 20',
-        refY: '50%',
-        refY2: -5
-      },
-      '.group-line': {
-        ref: '.outer',
-        refPointsKeepOffset: '1,42 169,42'
-      }
-    },
-    // custom properties
-    isOpen: true
-  }, joint.shapes.basic.Generic.prototype.defaults)
-});
+      // custom properties
+      isOpen: true
+    }, joint.shapes.basic.Generic.prototype.defaults)
+  });
+}
 
 export function createPaletteGroupHeader(title: string, isOpen: boolean) {
   const group = new joint.shapes.flo.DataflowPaletteGroupHeader();
@@ -141,5 +149,7 @@ export function shiftGraphHorizontallyOnPaper(paper: dia.Paper, offset: number) 
   const tx = offset > 0 && offset <= 1 ? (paper.$el.innerWidth() - box.width) * offset : offset;
   paper.translate(tx, currentTranslate.ty);
 }
+
+loadSharedShapes();
 
 
