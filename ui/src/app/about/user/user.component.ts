005eaebe99a62a5780f16b1651e2c331acb8584c
Damien Vitrac <dvitrac@vmware.com>
2020-08-26 16:58:53 +0200

    Update import absolute to relative

--- ui/src/app/about/user/user.component.ts
+++ ui/src/app/about/user/user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { SecurityService } from 'src/app/security/service/security.service';
+import { SecurityService } from '../../security/service/security.service';
 
 @Component({
   selector: 'app-user',
