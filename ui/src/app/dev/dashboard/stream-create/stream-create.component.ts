247e1015fb79f809e2726a65c536a686993711ac
Damien Vitrac <dvitrac@vmware.com>
2020-09-07 17:04:51 +0200

    Remove refresh icons

--- ui/src/app/dev/dashboard/stream-create/stream-create.component.ts
+++ ui/src/app/dev/dashboard/stream-create/stream-create.component.ts
@@ -5,7 +5,18 @@ import { delay, map, mergeMap } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
 import { NotificationService } from '../../../shared/service/notification.service';
 
-const STREAM_DESCRIPTION = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
+const STREAM_DESCRIPTION = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque '
+  + 'laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta '
+  + 'sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur '
+  + 'magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor '
+  + 'sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam '
+  + 'aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit '
+  + 'laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate '
+  + 'velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
+  + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore '
+  + 'magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo '
+  + 'consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. '
+  + 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 @Component({
   selector: 'app-dev-stream-create',
@@ -74,7 +85,7 @@ export class StreamCreateComponent {
   }
 
   generateStreamName(pattern: string, min: number, max: number) {
-    let random = +min;
+    let random = min;
     if (min < max) {
       random = Math.floor(Math.random() * (max - min + 1) + min);
     }
@@ -83,7 +94,7 @@ export class StreamCreateComponent {
   }
 
   generateStreamDescription(min: number, max: number) {
-    let random = +min;
+    let random = min;
     if (min < max) {
       random = Math.floor(Math.random() * (max - min + 1) + min);
     }
