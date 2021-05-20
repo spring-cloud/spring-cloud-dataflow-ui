2f09f835756c61fcc712981a91b66bb182f27698
Damien Vitrac <dvitrac@vmware.com>
2021-05-20 14:21:16 +0200

    Handle error on Cleanup modal

--- ui/src/app/tasks-jobs/tasks/cleanup/cleanup.component.ts
+++ ui/src/app/tasks-jobs/tasks/cleanup/cleanup.component.ts
@@ -3,6 +3,7 @@ import { TaskExecution } from "../../../shared/model/task-execution.model";
 import { TaskService } from "../../../shared/api/task.service";
 import { NotificationService } from "../../../shared/service/notification.service";
 import { Task } from "../../../shared/model/task.model";
+import { AppError } from "src/app/shared/model/error.model";
 
 @Component({
   selector: "app-task-cleanup",
@@ -14,7 +15,7 @@ export class CleanupComponent {
   loading = true;
   count: { completed: number; all: number };
   task: Task;
-  status = 'all';
+  status = "all";
   @Output() onCleaned = new EventEmitter();
 
   constructor(
@@ -23,39 +24,54 @@ export class CleanupComponent {
   ) {}
 
   open(task: Task) {
-    this.status = 'all';
+    this.status = "all";
     this.loading = true;
     this.task = task;
-    this.taskService.getTaskExecutionsCount(this.task).subscribe((count) => {
-      this.count = count;
-      this.loading = false;
-      if (this.count.all === 0) {
-        this.notificationService.warning('No execution', 'There is no execution for this task.');
+    this.taskService.getTaskExecutionsCount(this.task).subscribe(
+      (count) => {
+        this.count = count;
+        this.loading = false;
+        if (this.count.all === 0) {
+          this.notificationService.warning(
+            "No execution",
+            "There is no execution for this task."
+          );
+          this.isOpen = false;
+          this.task = null;
+        }
+      },
+      (error: AppError) => {
+        this.notificationService.error("An error occurred", error.getMessage());
         this.isOpen = false;
-        this.task = null;
       }
-    });
+    );
     this.isRunning = false;
     this.isOpen = true;
   }
 
   clean() {
     this.isRunning = true;
-    this.taskService.taskExecutionsClean(this.task, this.status === 'completed').subscribe(
-      () => {
-        this.notificationService.success(
-          "Clean up execution(s)",
-          `${this.status === 'completed' ? this.count.completed : this.count.all} execution(s) cleaned up.`
-        );
-        this.onCleaned.emit(this.count);
-        this.isOpen = false;
-        this.task = null;
-      },
-      (error) => {
-        this.notificationService.error("An error occurred", error);
-        this.isOpen = false;
-        this.task = null;
-      }
-    );
+    this.taskService
+      .taskExecutionsClean(this.task, this.status === "completed")
+      .subscribe(
+        () => {
+          this.notificationService.success(
+            "Clean up execution(s)",
+            `${
+              this.status === "completed"
+                ? this.count.completed
+                : this.count.all
+            } execution(s) cleaned up.`
+          );
+          this.onCleaned.emit(this.count);
+          this.isOpen = false;
+          this.task = null;
+        },
+        (error) => {
+          this.notificationService.error("An error occurred", error);
+          this.isOpen = false;
+          this.task = null;
+        }
+      );
   }
 }
