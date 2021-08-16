253a82320daa643188700e243d4b93fcf805471f
Damien Vitrac <oodamien@users.noreply.github.com>
2021-08-16 07:52:14 +0200

    Show id instead of name on App view
    - Fixes #3815

--- ui/src/app/apps/app/app.component.ts
+++ ui/src/app/apps/app/app.component.ts
@@ -10,10 +10,9 @@ import { HttpError } from '../../shared/model/error.model';
 
 @Component({
   selector: 'app-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-
   loading = true;
   loadingProperties = true;
   app: App;
@@ -21,46 +20,52 @@ export class AppComponent implements OnInit {
   defaultApp: App;
   selectedApp: App;
   detailedApp: DetailedApp;
-  @ViewChild('unregisterModal', { static: true }) unregisterModal: UnregisterComponent;
+  @ViewChild('unregisterModal', { static: true })
+  unregisterModal: UnregisterComponent;
   @ViewChild('versionModal', { static: true }) versionModal: VersionComponent;
   showAllProperties = true;
   tooManyProperties = false;
 
-  constructor(private route: ActivatedRoute,
-              private appsService: AppService,
-              private router: Router,
-              private notificationService: NotificationService) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private appsService: AppService,
+    private router: Router,
+    private notificationService: NotificationService
+  ) {}
 
   ngOnInit(): void {
-    this.route.params
-      .subscribe((params: Params) => {
-        this.app = App.parse({ name: params.appName, type: params.appType });
-        this.getVersions();
-      });
+    this.route.params.subscribe((params: Params) => {
+      this.app = App.parse({ name: params.appName, type: params.appType });
+      this.getVersions();
+    });
   }
 
   getVersions() {
     this.loading = true;
-    this.appsService
-      .getAppVersions(this.app.name, this.app.type)
-      .subscribe((apps: App[]) => {
-          if (apps.length === 0) {
-            this.notificationService.error('An error occurred', 'No application found.');
-            this.back();
-          }
-          this.versions = apps;
-          this.defaultApp = this.versions.find((a) => a.defaultVersion);
-          if (this.defaultApp) {
-            this.app = this.defaultApp;
-          }
-          this.changeVersion(this.defaultApp ? this.defaultApp : this.versions[0]);
-          this.loading = false;
-        },
-        error => {
-          this.notificationService.error('An error occurred', error);
+    this.appsService.getAppVersions(this.app.name, this.app.type).subscribe(
+      (apps: App[]) => {
+        if (apps.length === 0) {
+          this.notificationService.error(
+            'An error occurred',
+            'No application found.'
+          );
           this.back();
-        });
+        }
+        this.versions = apps;
+        this.defaultApp = this.versions.find((a) => a.defaultVersion);
+        if (this.defaultApp) {
+          this.app = this.defaultApp;
+        }
+        this.changeVersion(
+          this.defaultApp ? this.defaultApp : this.versions[0]
+        );
+        this.loading = false;
+      },
+      (error) => {
+        this.notificationService.error('An error occurred', error);
+        this.back();
+      }
+    );
   }
 
   getProperties(app: App) {
@@ -70,27 +75,28 @@ export class AppComponent implements OnInit {
       this.detailedApp.version = app.version;
       this.detailedApp.defaultVersion = app.defaultVersion;
     }
-    this.appsService.getApp(app.name, app.type, app.version)
-      .subscribe((detailedApp: DetailedApp) => {
-          this.tooManyProperties = (detailedApp.options.length > 50);
-          this.showAllProperties = !this.tooManyProperties;
-          this.detailedApp = detailedApp;
-          this.loadingProperties = false;
-        },
-        error => {
-          this.notificationService.error('An error occurred', error);
-          if (HttpError.is404(error)) {
-            this.back();
-          }
-          this.detailedApp = DetailedApp.parse({
-            name: app.name,
-            version: app.version,
-            versions: app.versions,
-            type: app.type,
-            defaultVersion: app.defaultVersion
-          });
-          this.loadingProperties = false;
+    this.appsService.getApp(app.name, app.type, app.version).subscribe(
+      (detailedApp: DetailedApp) => {
+        this.tooManyProperties = detailedApp.options.length > 50;
+        this.showAllProperties = !this.tooManyProperties;
+        this.detailedApp = detailedApp;
+        this.loadingProperties = false;
+      },
+      (error) => {
+        this.notificationService.error('An error occurred', error);
+        if (HttpError.is404(error)) {
+          this.back();
+        }
+        this.detailedApp = DetailedApp.parse({
+          name: app.name,
+          version: app.version,
+          versions: app.versions,
+          type: app.type,
+          defaultVersion: app.defaultVersion,
         });
+        this.loadingProperties = false;
+      }
+    );
   }
 
   changeVersion(app: App) {
@@ -112,5 +118,11 @@ export class AppComponent implements OnInit {
     this.versionModal.open(this.detailedApp.name, this.detailedApp.type);
   }
 
-
+  getReadableId(id: string): string {
+    const max = 60;
+    if (id.length > max) {
+      return `... ${id.substring(id.length - max, id.length)}`;
+    }
+    return id;
+  }
 }
