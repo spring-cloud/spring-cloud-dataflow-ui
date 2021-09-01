0d676b0646da3de194bb77a10567028d1df6e5a1
Damien Vitrac <dvitrac@vmware.com>
2021-09-01 09:49:03 +0200

    Simple properties (old version)

---
+++ ui/src/app/flo/task/simple-properties/simple-task-properties-source.ts
@@ -0,0 +1,36 @@
+import {Flo} from 'spring-flo';
+import {dia} from 'jointjs';
+import {GraphNodePropertiesSource} from '../../shared/support/graph-node-properties-source';
+import {AppUiProperty} from '../../shared/support/app-ui-property';
+import {ApplicationType} from '../../../shared/model/app.model';
+
+/**
+ * Properties source for Composed Tasks graph node
+ *
+ * @author Alex Boyko
+ */
+export class SimpleTaskGraphPropertiesSource extends GraphNodePropertiesSource {
+  protected createNotationalProperties(): Array<AppUiProperty> {
+    const notationalProperties = [];
+    if (typeof ApplicationType[this.cell.prop('metadata/group')] === 'number') {
+      notationalProperties.push({
+        id: 'label',
+        name: 'label',
+        defaultValue: this.cell.prop('metadata/name'),
+        attr: 'node-label',
+        value: this.cell.attr('node-label'),
+        description: 'Label of the task',
+        isSemantic: false
+      });
+    }
+    return notationalProperties;
+  }
+
+  protected determineAttributeName(metadata: Flo.PropertyMetadata): string {
+    if (this.cell instanceof dia.Link) {
+      // For links properties are always id based
+      return `props/${metadata.id}`;
+    }
+    return super.determineAttributeName(metadata);
+  }
+}
