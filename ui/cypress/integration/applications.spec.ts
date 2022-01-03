9d0575743ce2c478ab163ba91233bfcac862b26b
Claudia Bressi <cbressi@vmware.com>
2022-01-03 21:24:42 +0100

    finalize test applications group actions

--- ui/cypress/integration/applications.spec.ts
+++ ui/cypress/integration/applications.spec.ts
@@ -9,11 +9,20 @@ describe('Applications area', () => {
     cy.get('.datagrid clr-dg-cell').should('have.length.gte', 1)
   })
 
-  it('Test group applications', () => {
+  it('Test group actions', () => {
     cy.addApplications()
-    cy.get('button#btnGroupActions').click()
-    cy.get('input[type="checkbox"] + label').first().click()
-    cy.get('button#btnUnregisterApplications').click()
-    cy.get('.modal button').last().click()
+    cy.wait(1200)
+    cy.get('.apps-total').then($appTotal => {
+      const initialAddedApps = Number($appTotal.text());
+      cy.log('initialAddedApps 1', initialAddedApps)
+      cy.get('button#btnGroupActions').click()
+      cy.get('input[type="checkbox"] + label').first().click()
+      cy.get('button#btnUnregisterApplications').click()
+      cy.get('.modal button').last().click()
+      cy.wait(1200)
+      cy.get('.apps-total').then($appUpdatedTotal => {
+        expect(Number($appUpdatedTotal.text())).to.lt(initialAddedApps)
+      })
+    })
   })
 })
