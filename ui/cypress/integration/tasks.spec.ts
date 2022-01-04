4760d737218dabca93133efcbaf8418f6def823b
Claudia Bressi <cbressi@vmware.com>
2022-01-04 19:16:03 +0100

    Test launch and destroy tasks

--- ui/cypress/integration/tasks.spec.ts
+++ ui/cypress/integration/tasks.spec.ts
@@ -2,27 +2,39 @@ describe('Tasks area', () => {
   beforeEach(() => {
     cy.visit('/')
     cy.get('clr-vertical-nav-group[appfeature = "tasks"]').click()
-  })
-
-  it('Test created tasks rendered on table effectively', () => {
-    cy.importTasks()
-    cy.get('a[routerlink = "tasks-jobs/tasks"]').first().click()
-    cy.createTasks()
-    cy.get('.datagrid clr-dg-cell').should('have.length.gte', 1)
-  })
-
-  it('Test view details for selected task', () => {
     cy.importTasks()
     cy.get('a[routerlink = "tasks-jobs/tasks"]').first().click()
     cy.createTasks()
     cy.wait(1200)
-    cy.get('.tasks-total').then($tasksTotal => {
-      const initialTasks = $tasksTotal.text();
-      cy.get('.datagrid-action-toggle').last().click()
-      cy.wait(1200)
-      cy.get('.datagrid-action-overflow button').first().click()
-      cy.wait(1200)
-      cy.get('app-view-card').should('have.id','info')
-    })
   })
+
+  it('Test created tasks rendered on table effectively', () => {
+    cy.get('.datagrid clr-dg-cell').should('have.length.gte', 1)
+  })
+
+  it('Test view details for selected task', () => {
+    cy.get('.datagrid-action-toggle').last().click()
+    cy.wait(1200)
+    cy.get('.datagrid-action-overflow button').first().click()
+    cy.wait(1200)
+    cy.get('app-view-card').should('have.id','info')
+  })
+
+  it('Test launch selected task', () => {
+    cy.get('.datagrid-action-toggle').last().click()
+    cy.wait(1200)
+    cy.get('.datagrid-action-overflow button:nth-child(2)').click()
+    cy.wait(1200)
+    cy.get('button#btn-deploy-builder').click()
+    cy.get('app-view-card').invoke('attr', 'keycontext').should('eq','execution')
+  })
+
+  it('Test destroy selected task', () => {
+    cy.get('.datagrid-action-toggle').last().click()
+    cy.wait(1200)
+    cy.get('.datagrid-action-overflow button:nth-child(4)').first().click()
+    cy.wait(1200)
+    cy.get('.modal-dialog button').last().click()
+  })
+
 })
