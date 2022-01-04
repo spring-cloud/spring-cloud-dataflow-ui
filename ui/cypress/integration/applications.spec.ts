a082ab863a39e5752765d716f6e8791af78a141b
Claudia Bressi <cbressi@vmware.com>
2022-01-04 11:11:32 +0100

    Verify tests on view details for selected application

--- ui/cypress/integration/applications.spec.ts
+++ ui/cypress/integration/applications.spec.ts
@@ -9,19 +9,45 @@ describe('Applications area', () => {
     cy.get('.datagrid clr-dg-cell').should('have.length.gte', 1)
   })
 
-  it('Test group actions', () => {
+  it('Test unregister for selected application', () => {
     cy.addApplications()
-    cy.wait(1200)
+    cy.wait(1000)
+    cy.get('.apps-total').then($appTotal => {
+      const initialAddedApps = $appTotal.text();
+      cy.get('.datagrid-action-toggle').first().click()
+      cy.wait(1200)
+      cy.get('.datagrid-action-overflow button.action-item').last().click()
+      cy.wait(1200)
+      cy.get('.apps-total').then($appUpdatedTotal => {
+        expect(Number($appUpdatedTotal.text())).to.eq(Number(initialAddedApps) - 1)
+      })
+    })
+  })
+
+  it('Test show details for selected application', () => {
+    cy.addApplications()
+    cy.wait(1000)
     cy.get('.apps-total').then($appTotal => {
       const initialAddedApps = Number($appTotal.text());
-      cy.log('initialAddedApps 1', initialAddedApps)
+      cy.get('.datagrid-action-toggle').last().click()
+      cy.wait(1000)
+      cy.get('.datagrid-action-overflow button.action-item').last().click()
+      cy.wait(1000)
+    })
+  })
+
+  it('Test group actions', () => {
+    cy.addApplications()
+    cy.wait(1000)
+    cy.get('.apps-total').then($appTotal => {
+      const initialAddedApps = $appTotal.text();
       cy.get('button#btnGroupActions').click()
       cy.get('input[type="checkbox"] + label').first().click()
       cy.get('button#btnUnregisterApplications').click()
       cy.get('.modal button').last().click()
-      cy.wait(1200)
+      cy.wait(1000)
       cy.get('.apps-total').then($appUpdatedTotal => {
-        expect(Number($appUpdatedTotal.text())).to.lt(initialAddedApps)
+        expect(Number($appUpdatedTotal.text())).to.lt(Number(initialAddedApps))
       })
     })
   })
