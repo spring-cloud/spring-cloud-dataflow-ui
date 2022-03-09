4508d3dfe502d270caa8d0520703cd3894771ad2
Claudia Bressi <cbressi@vmware.com>
2022-03-09 12:51:41 +0100

    Fix linting

--- ui/cypress/integration/streams.spec.ts
+++ ui/cypress/integration/streams.spec.ts
@@ -53,13 +53,15 @@ describe('Streams', () => {
     cy.get('.datagrid-action-toggle').first().click();
     cy.get('.datagrid-action-overflow button:nth-child(3)').first().click();
     cy.get('table input[type=number]').should('be.exist');
-    cy.get('table input[type=number]').first().then(appNameElement => {
-      const appName = appNameElement.parent().prev('td').text();
-      cy.get('table input[type=number]').eq(0).clear().type('2');
-      cy.get('table input[type=number]').eq(0).parent().next('td').children('button').first().click();
-      cy.shouldShowToast('Scale stream', `${appName} app scaled to 2.`);
-      cy.get('button.close').click();
-    });
+    cy.get('table input[type=number]')
+      .first()
+      .then(appNameElement => {
+        const appName = appNameElement.parent().prev('td').text();
+        cy.get('table input[type=number]').eq(0).clear().type('2');
+        cy.get('table input[type=number]').eq(0).parent().next('td').children('button').first().click();
+        cy.shouldShowToast('Scale stream', `${appName} app scaled to 2.`);
+        cy.get('button.close').click();
+      });
   });
 
   it('should prevent scaling to negative instances', () => {
