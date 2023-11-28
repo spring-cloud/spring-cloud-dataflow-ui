d750ca7d1683b7d2f887371a3dbf1ff644abc312
Damien Vitrac <dvitrac@vmware.com>
2023-11-28 16:36:51 +0100

    Update task schedule validator
    Resolves #1974

--- ui/src/app/tasks-jobs/tasks/task-prop.validator.ts
+++ ui/src/app/tasks-jobs/tasks/task-prop.validator.ts
@@ -3,7 +3,13 @@ import {UntypedFormControl} from '@angular/forms';
 export class TaskPropValidator {
   static key(control: UntypedFormControl): any {
     const value = control.value;
-    if (value && !value.startsWith('app.') && !value.startsWith('deployer.') && !value.startsWith('scheduler.')) {
+    if (
+      value &&
+      !value.startsWith('app.') &&
+      !value.startsWith('deployer.') &&
+      !value.startsWith('scheduler.') &&
+      !value.startsWith('version.')
+    ) {
       return {invalid: true};
     }
     return null;
